cf4592ed1fe5ef31cb9e6eee4139e62cb0b49c4e
RinHye <pauline.lam24@gmail.com>
2021-03-25 22:18:21 +0000

    Deploying to gh-pages from master @ d21b39b3381d31d35593cb14c5d0d4b9dd2c23d7 🚀

--- Cinéma.tsx
+++ Cinéma.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Cinéma" tilewidth="32" tileheight="32" tilecount="532" columns="38">
- <image source="Cinéma.png" trans="000000" width="1216" height="471"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Cinéma" tilewidth="32" tileheight="32" tilecount="540" columns="35">
+ <image source="Cinéma.png" trans="000000" width="1150" height="485"/>
 </tileset>
